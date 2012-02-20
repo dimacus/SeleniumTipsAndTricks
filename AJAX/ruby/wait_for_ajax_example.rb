a5a8c3ba9011bf6fcb9f8ac020a7070731a10a05
Dima Kovalenko <dima@groupon.com>
2012-02-20 14:30:50 -0500

    Reduce the sleep time for wait for AJAX, and put the driver.quit into an ensure block

--- AJAX/ruby/wait_for_ajax_example.rb
+++ AJAX/ruby/wait_for_ajax_example.rb
@@ -6,18 +6,18 @@ require "selenium-webdriver"
 def wait_for_ajax
   begin
     puts "Waiting for AJAX to complete"
-    sleep 1
+    sleep 0.3
   end while (@driver.execute_script("return jQuery.active") != 0)
     puts "\n\nAJAX loaded!!\n\n"
 end
 
 
-
-@driver.navigate.to "http://www.reddit.com/"
-@driver.find_element(:name, "user").send_keys("selenium_test_user")
-@driver.find_element(:name, "passwd").send_keys("123456")
-@driver.find_element(:name, "passwd").submit
-wait_for_ajax
-
-
-@driver.quit
+begin
+  @driver.navigate.to "http://www.reddit.com/"
+  @driver.find_element(:name, "user").send_keys("selenium_test_user")
+  @driver.find_element(:name, "passwd").send_keys("123456")
+  @driver.find_element(:name, "passwd").submit
+  wait_for_ajax
+ensure
+  @driver.quit
+end
